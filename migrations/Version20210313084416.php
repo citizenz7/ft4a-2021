cdab31304c9f2e9648b6f60566cddfc2c6de6c65
RICHARD Eddy <richard.eddy@live.fr>
2021-03-13 09:49:11 +0100

    fix: update initial migrations

--- migrations/Version20210313084416.php
+++ migrations/Version20210313084416.php
@@ -7,23 +7,19 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-final class Version20210223181146 extends AbstractMigration
+final class Version20210313084416 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return '';
+        return 'Initial schema';
     }
 
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE categories (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE comments (id INT AUTO_INCREMENT NOT NULL, author_id INT NOT NULL, torrent_id INT DEFAULT NULL, parent_id INT DEFAULT NULL, content LONGTEXT NOT NULL, date DATETIME NOT NULL, active TINYINT(1) NOT NULL, INDEX IDX_5F9E962AF675F31B (author_id), INDEX IDX_5F9E962A9162822 (torrent_id), INDEX IDX_5F9E962A727ACA70 (parent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE licences (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE members (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(50) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, pid VARCHAR(50) NOT NULL, date DATETIME NOT NULL, avatar VARCHAR(255) DEFAULT NULL, signature VARCHAR(255) DEFAULT NULL, active TINYINT(1) DEFAULT \'1\' NOT NULL, is_verified TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_45A0D2FFF85E0677 (username), UNIQUE INDEX UNIQ_45A0D2FFE7927C74 (email), UNIQUE INDEX UNIQ_45A0D2FF5550C4ED (pid), UNIQUE INDEX UNIQ_45A0D2FF1677722F (avatar), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE members (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(50) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, pid VARCHAR(50) NOT NULL, date DATETIME NOT NULL, avatar VARCHAR(255) DEFAULT NULL, signature VARCHAR(255) DEFAULT NULL, active TINYINT(1) DEFAULT \'1\' NOT NULL, is_verified TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_45A0D2FFF85E0677 (username), UNIQUE INDEX UNIQ_45A0D2FFE7927C74 (email), UNIQUE INDEX UNIQ_45A0D2FF5550C4ED (pid), UNIQUE INDEX UNIQ_45A0D2FF1677722F (avatar), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE torrents (id INT AUTO_INCREMENT NOT NULL, author_id INT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, hash VARCHAR(40) NOT NULL, link VARCHAR(255) DEFAULT NULL, content LONGTEXT NOT NULL, size BIGINT NOT NULL, date DATETIME NOT NULL, torrent_file VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, views INT NOT NULL, INDEX IDX_39D01E05F675F31B (author_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE torrents_categories (torrents_id INT NOT NULL, categories_id INT NOT NULL, INDEX IDX_9F0DA2663218EAE9 (torrents_id), INDEX IDX_9F0DA266A21214B7 (categories_id), PRIMARY KEY(torrents_id, categories_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -41,7 +37,6 @@ final class Version20210223181146 extends AbstractMigration
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE torrents_categories DROP FOREIGN KEY FK_9F0DA266A21214B7');
         $this->addSql('ALTER TABLE comments DROP FOREIGN KEY FK_5F9E962A727ACA70');
         $this->addSql('ALTER TABLE torrents_licences DROP FOREIGN KEY FK_E6C4DA595EF2836');
